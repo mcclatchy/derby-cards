d0e86ecad6371ab11f6787981296be44f7b37501
Kristi Walker <kristiwalker22@gmail.com>
2017-04-27 10:04:23 -0400

    adding files

---
+++ build/static/scripts/base.js
@@ -0,0 +1,12 @@
+var targetModel = 1;
+var currentModel = 0;
+var loopLength;
+
+/***************************************************************************
+Gets model playlist
+***************************************************************************/
+
+$(document).ready(function() {
+    $('#dia-turntable').turntable();
+    $('#dia2-turntable').turntable();
+});
