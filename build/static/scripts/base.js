bfab064bac279e8d3814cac5a8d355438cc1fa93
Kristi Walker <kristiwalker22@gmail.com>
2017-04-27 22:14:22 -0400

    adds custom lazy-load-esque scripts

--- build/static/scripts/base.js
+++ build/static/scripts/base.js
@@ -2,6 +2,7 @@ var targetModel = 1;
 var currentModel = 0;
 var loopLength;
 
+
 /***************************************************************************
 Gets model playlist
 ***************************************************************************/
@@ -9,4 +10,39 @@ Gets model playlist
 $(document).ready(function() {
     $('#dia-turntable').turntable();
     $('#dia2-turntable').turntable();
+    $('#dia3-turntable').turntable();
+});
+
+/* Every time the window is scrolled ... */
+$(window).scroll( function(){
+
+    /* Check the location of each desired element */
+    $('.hc-card').each(function(i){
+
+        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height() + 800;
+        console.log(bottom_of_object, bottom_of_window);
+
+        var turntableId = $(this).children(":first").attr('id');
+
+        /* If the object is completely visible in the window, fade it in */
+        if (bottom_of_window > bottom_of_object) {
+            if ($('#' + turntableId + ' ul li.hc-end').length === 0) {
+                console.log('test');
+
+                var test = "<li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-22.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-21.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-20.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-19.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-18.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-17.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-16.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-15.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-14.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-13.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-12.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-11.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-10.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-9.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-8.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-7.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-6.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-5.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-4.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-3.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-2.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-1.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-23.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-22.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-21.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-20.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-19.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-18.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-17.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-16.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-15.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-14.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-13.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-12.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-11.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-10.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-9.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-8.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-7.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-6.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-5.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-4.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-3.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-2.png><li data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-1.png><li class='hc-end' data-img-src=static/assets/images/dia-hat-1000/dia-hat-1000-23.png>";
+
+                $('#' + turntableId + ' ul').append(test);
+                $('#' + turntableId).turntable();
+            }
+
+            // $('#' + turntableId).turntable();
+            // $(this).children(":first").animate({'opacity':'1'},500);
+
+            // $(this).animate({'opacity':'1'},3000);
+
+        }
+
+    });
+
 });
--- build/static/scripts/old-player.js
+++ build/static/scripts/old-player.js
@@ -25,6 +25,29 @@ var Player = function(playlist) {
 
   // Display the title of the first track.
   track.innerHTML = playlist[0].title;
+
+  // Setup the playlist display.
+  // playlist.forEach(function(audio) {
+  //   var listSong = document.createElement('div');
+  //   var listThumb = document.createElement('div');
+  //   var listHead = document.createElement('div');
+  //
+  //   listSong.className = 'list-song';
+  //   listThumb.className = 'list-thumb';
+  //
+  //   listSong.appendChild(listThumb);
+  //   listThumb.appendChild(listHead);
+  //   listHead.innerHTML = audio.title;
+  //   // listSong.innerHTML = audio.title;
+  //   listSong.onclick = function() {
+  //     player.skipTo(playlist.indexOf(audio));
+  //
+  //     // STOPPED HERE, NEEDS TO CHANGE QUEUE ASSETS OR COMBINE FILES
+  //     queueAssets(currentModel, player);
+  //     loopLength = data.length;
+  //   };
+  //   list.appendChild(listSong);
+  // });
 };
 Player.prototype = {
   /**
@@ -161,6 +184,17 @@ Player.prototype = {
    * Set the volume and update the volume slider display.
    * @param  {Number} val Volume between 0 and 1.
    */
+  // volume: function(val) {
+  //   var self = this;
+  //
+  //   // Update the global volume (affecting all Howls).
+  //   Howler.volume(val);
+  //
+  //   // Update the display on the slider.
+  //   var barWidth = (val * 90) / 100;
+  //   barFull.style.width = (barWidth * 100) + '%';
+  //   sliderBtn.style.left = (window.innerWidth * barWidth + window.innerWidth * 0.05 - 25) + 'px';
+  // },
 
   /**
    * Seek to a new position in the currently playing track.
@@ -214,6 +248,19 @@ Player.prototype = {
   },
 
   /**
+   * Toggle the volume display on/off.
+   */
+  // toggleVolume: function() {
+  //   var self = this;
+  //   var display = (volume.style.display === 'block') ? 'none' : 'block';
+  //
+  //   setTimeout(function() {
+  //     volume.style.display = display;
+  //   }, (display === 'block') ? 0 : 500);
+  //   volume.className = (display === 'block') ? 'fadein' : 'fadeout';
+  // },
+
+  /**
    * Format the time from seconds to M:SS.
    * @param  {Number} secs Seconds to format.
    * @return {String}      Formatted time.
@@ -255,7 +302,97 @@ playBtn.addEventListener('click', function() {
 pauseBtn.addEventListener('click', function() {
   player.pause();
 });
-
+// prevBtn.addEventListener('click', function() {
+//   player.skip('prev');
+// });
+// nextBtn.addEventListener('click', function() {
+//   player.skip('next');
+// });
+// waveform.addEventListener('click', function(event) {
+//   player.seek(event.clientX / window.innerWidth);
+// });
 scrubber.addEventListener('click', function(event) {
   player.seek(event.clientX / (scrubber.clientWidth));
 });
+
+// playlistBtn.addEventListener('click', function() {
+//   player.togglePlaylist();
+// });
+// playlist.addEventListener('click', function() {
+//   player.togglePlaylist();
+// });
+// volumeBtn.addEventListener('click', function() {
+//   player.toggleVolume();
+// });
+// volume.addEventListener('click', function() {
+//   player.toggleVolume();
+// });
+
+// Setup the event listeners to enable dragging of volume slider.
+// barEmpty.addEventListener('click', function(event) {
+//   var per = event.layerX / parseFloat(barEmpty.scrollWidth);
+//   player.volume(per);
+// });
+// sliderBtn.addEventListener('mousedown', function() {
+//   window.sliderDown = true;
+// });
+// sliderBtn.addEventListener('touchstart', function() {
+//   window.sliderDown = true;
+// });
+// volume.addEventListener('mouseup', function() {
+//   window.sliderDown = false;
+// });
+// volume.addEventListener('touchend', function() {
+//   window.sliderDown = false;
+// });
+
+// var move = function(event) {
+//   if (window.sliderDown) {
+//     var x = event.clientX || event.touches[0].clientX;
+//     var startX = window.innerWidth * 0.05;
+//     var layerX = x - startX;
+//     var per = Math.min(1, Math.max(0, layerX / parseFloat(barEmpty.scrollWidth)));
+//     player.volume(per);
+//   }
+// };
+
+// volume.addEventListener('mousemove', move);
+// volume.addEventListener('touchmove', move);
+
+// Setup the "waveform" animation.
+// var wave = new SiriWave({
+//     container: waveform,
+//     width: window.innerWidth,
+//     height: window.innerHeight * 0.3,
+//     cover: true,
+//     speed: 0.03,
+//     amplitude: 0.7,
+//     frequency: 2
+// });
+// wave.start();
+
+// Update the height of the wave animation.
+// These are basically some hacks to get SiriWave.js to do what we want.
+// var resize = function() {
+//   var height = window.innerHeight * 0.3;
+//   var width = window.innerWidth;
+//   wave.height = height;
+//   wave.height_2 = height / 2;
+//   wave.MAX = wave.height_2 - 4;
+//   wave.width = width;
+//   wave.width_2 = width / 2;
+//   wave.width_4 = width / 4;
+//   wave.canvas.height = height;
+//   wave.canvas.width = width;
+//   wave.container.style.margin = -(height / 2) + 'px auto';
+//
+//   // Update the position of the slider.
+//   // var sound = player.playlist[player.index].howl;
+//   // if (sound) {
+//   //   var vol = sound.volume();
+//   //   var barWidth = (vol * 0.9);
+//   //   sliderBtn.style.left = (window.innerWidth * barWidth + window.innerWidth * 0.05 - 25) + 'px';
+//   // }
+// };
+// window.addEventListener('resize', resize);
+// resize();
